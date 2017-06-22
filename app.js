deb893b5b0660c165cde28d27fdfbd8e5fc091ca
SoriamoriaDev <maxime.fontanille@gmail.com>
2017-06-22 16:20:18 +0200

    fixed the .env file problem

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env' });
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.load({path: '.env'})
+}
 
 /**
  * Controllers (route handlers).
