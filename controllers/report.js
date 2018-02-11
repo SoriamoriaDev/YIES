e66c7c6b1bdd237ad69dfd158b913858a48a7fad
SoriamoriaDev <maxime.fontanille@gmail.com>
2018-02-11 09:11:21 +0100

    excuse to commit

--- controllers/report.js
+++ controllers/report.js
@@ -145,6 +145,7 @@ exports.getReport = (req, res, next) => {
         strength.push(report3[2]);
         // console.log(strength);
 
+        
         // SORT WORSE TO BEST
         report3.sort(function (a, b) {
             return a[1] > b[1] ? 1 : -1;
